5faadd9b6abf5d649747891eabddaa95ecddef8c
mihirpatil0 <mihirrpatil@gmail.com>
2021-06-29 02:48:18 +0530

    [mihir] Added: Required variables thier getter, setter method and overriding toString() method.

--- src/com/bridgelab/addressbook/Contacts.java
+++ src/com/bridgelab/addressbook/Contacts.java
@@ -12,5 +12,69 @@ package com.bridgelab.addressbook;
 
 public class Contacts
 {
+	private String firstName;
+	private String lastName;
+	private String address;
+	private String city;
+	private String state;
+	private int zipCode;
+	private long phoneNumber;
+	private String emailId;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public int getZipCode() {
+		return zipCode;
+	}
+	public void setZipCode(int zipCode) {
+		this.zipCode = zipCode;
+	}
+	public long getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(long phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
 
+	
+	  @Override public String toString() 
+	  { 
+		  return "Contacts [firstName=" + firstName + ", lastName=" + lastName + ", address=" + address + ", city=" +
+				  city + ", state=" + state + ", zipCode=" + zipCode + ", phoneNumber=" +
+				  phoneNumber + ", emailId=" + emailId + "]";
+	  }
 }
