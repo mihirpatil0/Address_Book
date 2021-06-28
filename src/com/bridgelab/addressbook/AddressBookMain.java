f0b027c2faf7a15fc59351ee6bbb538deb45ac6f
mihirpatil0 <mihirrpatil@gmail.com>
2021-06-29 03:08:56 +0530

    [mihir] Added: create/add new contact method that will store new entry of contact in addressbook.

--- src/com/bridgelab/addressbook/AddressBookMain.java
+++ src/com/bridgelab/addressbook/AddressBookMain.java
@@ -1,5 +1,7 @@
-
 package com.bridgelab.addressbook;
+
+import java.util.Scanner;
+import java.util.ArrayList;
 
 /**************************************************************************
  * @author mihir
@@ -14,8 +16,47 @@ package com.bridgelab.addressbook;
 
 public class AddressBookMain
 {
+	//Scanner object creation.
+	public Scanner scannerObject = new Scanner(System.in);
+	//ArrayList object creation
+	ArrayList<Contacts> contactsArrayList = new ArrayList<Contacts>();
+	//Instantiating Service class object and in constructor passing scanner object.
+	Service serviceObject = new Service(scannerObject);
+	
+	/**
+	 * Name : chooseOption
+	 * 
+	 * Description : Displaying option to user to choose from.
+	 * 
+	 * Algorithm : In this method displaying user multiple option from switch statement.
+	 * according to users choice respective method will get called.
+	 * 
+	 * Modification : First commit 28-June-2021
+	 */
+	public void chooseOption()
+	{
+		boolean isExit = false;
+	    
+	    while (!isExit) 
+	    {
+	      System.out.print("Enter options 1.Add-New Contacts : ");
+		  int userInput = scannerObject.nextInt();
+		  
+	      switch (userInput)
+	      	{
+	        case 1:
+	        		// Add New-Contact details.
+	        		contactsArrayList.add(serviceObject.addNewContact());
+	        		break;
+	        default:
+	        		System.out.println("Please enter valid input");
+			}
+		}
+	}
+	
 	public static void main(String[] args)
 	{
-		System.out.println("Welcome to Address Book Program !");
+		AddressBookMain addressBookMainObject = new AddressBookMain();
+		addressBookMainObject.chooseOption();
 	}
 }
--- src/com/bridgelab/addressbook/Service.java
+++ src/com/bridgelab/addressbook/Service.java
@@ -1,5 +1,6 @@
-  
 package com.bridgelab.addressbook;
+
+import java.util.Scanner;
 
 /************************************************************************
  * @author mihir
@@ -12,5 +13,62 @@ package com.bridgelab.addressbook;
 
 public class Service
 {
-
+	Scanner scannerObject;
+	//Creating object of Contacts class.
+	Contacts contactsObject = new Contacts();
+	
+	
+	/**
+	 * Name : Service ( Constructor )
+	 * 
+	 * Description : Passing scanner object in a constructor.
+	 * Scanner object is being passed from {@link AddressBookMain} class to {@link Service} class.
+	 * 
+	 * @param scannerObject
+	 */
+	public Service(Scanner scannerObject)
+	{
+		this.scannerObject = scannerObject;
+	}
+	
+	/**
+	 * Name : addNewContact
+	 * 
+	 * Description : Adding new contact to AddressBook.
+	 * 
+	 * Algorithm : In this method accepting all necessary contact details from user,
+	 * and returning those all details to store in AddressBook as a new contact.
+	 *  
+	 * @return Contacts object type which holds all the details.
+	 * 
+	 * Modification : First commit 28-June-2021.
+	 */
+	public Contacts addNewContact()
+	{
+		System.out.print("Enter First Name : ");
+		contactsObject.setFirstName(scannerObject.next());
+		
+		System.out.print("Enter Lirst Name : ");
+		contactsObject.setLastName(scannerObject.next());
+		
+		System.out.print("Enter Address : ");
+		contactsObject.setAddress(scannerObject.next());
+		
+		System.out.print("Enter City : ");
+		contactsObject.setCity(scannerObject.next());
+		
+		System.out.print("Enter State : ");
+		contactsObject.setState(scannerObject.next());
+		
+		System.out.print("Enter ZipCode : ");
+		contactsObject.setZipCode(scannerObject.nextInt());
+		
+		System.out.print("Enter Phone-Number : ");
+		contactsObject.setPhoneNumber(scannerObject.nextLong());
+		
+		System.out.print("Enter Email-Id : ");
+		contactsObject.setEmailId(scannerObject.next());
+		
+		return contactsObject;
+	}
 }
